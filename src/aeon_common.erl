9a381b7d0fd92397f186e8d406872b4fb7422e59
Garret Smith <garret.smith@gmail.com>
2015-05-15 16:29:48 -0700

    Support deep nesting with many alternatives

--- src/aeon_common.erl
+++ src/aeon_common.erl
@@ -35,6 +35,7 @@ first_no_fail(F, [A | Args]) ->
 	catch
 		error:{no_type, T} -> error({no_type, T});
 		throw:{no_conversion, _V, _T} -> first_no_fail(F, Args);
+		throw:all_failed -> first_no_fail(F, Args); % first_no_fail can be nested
 		throw:try_again -> first_no_fail(F, Args)
 	end.
 
